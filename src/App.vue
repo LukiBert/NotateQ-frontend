895736ccd079736f1dcf2f3445e1c48977792511
Bartosz Daniluk <bartek1.daniluk@gmail.com>
2025-03-30 22:57:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SearchBar from './components/SearchBar.vue'
+
+const searchInput = ref('')
+
+function receiveEmit(receivedText: string) {
+  searchInput.value = receivedText
+}
+</script>
+
+<template>
+  <SearchBar @search-phrase="receiveEmit" />
+
+  <main>Input: {{ searchInput }}</main>
+</template>
+
+<style scoped>
+main {
+  color: black;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { MdSearch } from '@vicons/ionicons4'
+import { NInputGroup, NInput, NButton, NIcon } from 'naive-ui'
+
+const emit = defineEmits(['search-phrase'])
+
+const searchPhrase = ref('')
+
+function emitSearchPhrase() {
+  emit('search-phrase', searchPhrase.value)
+  searchPhrase.value = ''
+}
+</script>
+
+<template>
+  <n-input-group>
+    <n-input round v-model:value="searchPhrase" @keyup.enter="emitSearchPhrase" />
+    <n-button circle @click="emitSearchPhrase">
+      <n-icon>
+        <MdSearch />
+      </n-icon>
+    </n-button>
+  </n-input-group>
+</template>
