529558937da753251a6f90c986b1b1b2af44e357
Bartosz Daniluk <bartek1.daniluk@gmail.com>
2025-04-24 09:01:47 +0200

    Select category from query works (with little bug)

--- src/views/SearchPageView.vue
+++ src/views/SearchPageView.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { NFlex, NSelect } from 'naive-ui'
+import { ref, onMounted, computed, nextTick } from 'vue'
+import { type Category, getAllCategories } from '../constants'
 
 const route = useRoute()
 
-const category = route.query.category as string | undefined
+const fetchedCategories = ref<Category[]>([])
+const selectedCategories = ref([])
+
+const queryCategories = computed(() => {
+  const raw = route.query.category
+  if (Array.isArray(raw)) return raw
+  if (typeof raw === 'string') return [raw]
+  return []
+})
+
+const categoriesWithLabels = computed(() => {
+  return fetchedCategories.value.map((cat) => ({
+    label: cat.name,
+    value: cat.id,
+  }))
+})
+
+onMounted(async () => {
+  try {
+    fetchedCategories.value = await getAllCategories()
+
+    selectedCategories.value = queryCategories.value
+  } catch (err) {
+    console.error('Failed to load categories')
+  }
+})
 </script>
 
-<template>{{ category }}</template>
+<template>
+  <n-flex vertical>
+    <n-select v-model:value="selectedCategories" multiple :options="categoriesWithLabels" />
+  </n-flex>
+</template>
