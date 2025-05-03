86768491a03a8aed6e3040872cd08ee5c2c46e81
Bartosz Daniluk <bartek1.daniluk@gmail.com>
2025-05-03 21:53:01 +0200

    add components to filter form

--- src/components/FiltersForm.vue
+++ src/components/FiltersForm.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { NSelect } from 'naive-ui'
+import { NSelect, NDatePicker, NFlex, NInputNumber, NDynamicTags } from 'naive-ui'
 import { ref, onMounted, computed, watch } from 'vue'
 import { type Category, getFilesData } from '../constants'
+import { FeedFilled } from '@vicons/material'
 
 const route = useRoute()
 
 const fetchedCategories = ref<Category[]>([])
 const selectedCategories = ref([])
+
+const timestamp = ref<[number, number]>([1183135260000, Date.now()])
+const minRate = ref(0)
+const maxRate = ref(5)
+const tags = ref(['myTag', 'fiz2'])
 
 const queryCategories = computed(() => {
   const raw = route.query.category
@@ -25,15 +31,33 @@ const categoriesWithLabels = computed(() => {
 </script>
 
 <template>
-  <div class="options-wrapper">
+  <n-flex>
     <n-select v-model:value="selectedCategories" multiple :options="categoriesWithLabels" />
     <n-select placeholder="Sortuj"></n-select>
-  </div>
+    <n-date-picker v-model:value="timestamp" type="daterange" clearable />
+    <n-input-number
+      v-model:value="minRate"
+      placeholder="Min"
+      :min="0"
+      :max="maxRate"
+      :step="0.1"
+      button-placement="both"
+    />
+    <n-input-number
+      v-model:value="maxRate"
+      placeholder="Max"
+      :min="minRate"
+      :max="5"
+      :step="0.1"
+      button-placement="both"
+    />
+    <n-dynamic-tags v-model:value="tags" />
+  </n-flex>
 </template>
 
 <style scoped>
-.options-wrapper {
-  width: 500px;
-  display: flex;
+.n-input-number {
+  width: 100px;
+  text-align: center;
 }
 </style>
